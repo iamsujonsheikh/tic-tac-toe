3548d4c58ba0f1b49bbef362d8743c4c0789f884
iamsujonsheikh <sujonsheikh.in@gmail.com>
2024-02-28 14:52:48 +0600

    project initialize done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl font-bold text-pink-500">
+        Hello developer
+      </h1>
+    </>
+  );
+}
+
+export default App;

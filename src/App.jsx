0789b0072867a74dd082b8ff52465cc64c7b69e2
iamsujonsheikh <sujonsheikh.in@gmail.com>
2024-02-28 16:41:00 +0600

    done props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-function App() {
+import { useState } from "react";
+
+// square function
+const Square = () => {
+  const [value, setValue] = useState(null);
+  // handle function
+  const handleClick = () => {
+    setValue("X");
+  };
+
+  return (
+    <button
+      className="bg-white border border-gray-400 rounded h-12 w-12 m-1 leading-9 tex-lg"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+};
+
+// main components
+const Board = () => {
   return (
     <>
-      <h1 className="text-center text-4xl font-bold text-pink-500">
-        Hello developer
-      </h1>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
-}
-
-export default App;
+};
+export default Board;

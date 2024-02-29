553f1fa44ea21d64c2b5bdd50644e57d29d40ddc
iamsujonsheikh <sujonsheikh.in@gmail.com>
2024-02-29 12:33:53 +0600

    start logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from "react";
 
 // square function
-const Square = () => {
-  const [value, setValue] = useState(null);
+const Square = ({ value, onSquareClick }) => {
   // handle function
-  const handleClick = () => {
-    setValue("X");
-  };
 
   return (
     <button
       className="bg-white border border-gray-400 rounded h-12 w-12 m-1 leading-9 tex-lg"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -20,24 +16,31 @@ const Square = () => {
 
 // main components
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    squares[0] = "x";
+    setSquares(squares);
+  }
+
   return (
     <>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
 
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
 
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
